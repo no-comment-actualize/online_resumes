b6a920edca2be55c1a4b0b5eb6a083b2955905e1
eddie <eddielee001@gmail.com>
2019-10-31 11:43:05 -0500

    adds dynamic code and remove hard coded value for models

--- src/views/ResumesShow.vue
+++ src/views/ResumesShow.vue
@@ -13,27 +13,35 @@
     <p>{{ student.photo }}</p>
 
     <h2>EXPERIENCE</h2>
-    <p>{{ student.experiences.start_date }}</p>
-    <p>{{ student.experiences.end_date }}</p>
-    <p>{{ student.experiences.job_title }}</p>
-    <p>{{ student.experiences.company }}</p>
-    <p>{{ student.experiences.details }}</p>
+    <p>{{ student.experiences[0]["start_date"] }}</p>
+    <p>{{ student.experiences[0]["end_date"] }}</p>
+    <p>{{ student.experiences[0]["job_title"] }}</p>
+    <p>{{ student.experiences[0]["details"] }}</p>
 
     <h2>EDUCATION</h2>
-    <p>{{ education.start_date }}</p>
-    <p>{{ education.end_date }}</p>
-    <p>{{ education.degree }}</p>
-    <p>{{ education.university }}</p>
-    <p>{{ education.details }}</p>
+    <p>{{ student.educations[0]["start_date"] }}</p>
+    <p>{{ student.educations[0]["end_date"] }}</p>
+    <p>{{ student.educations[0]["degree"] }}</p>
+    <p>{{ student.educations[0]["university"] }}</p>
+    <p>{{ student.educations[0]["details"] }}</p>
 
     <h2>SKILLS</h2>
-    <p>{{ skill.name }}</p>
-
+    <p>{{ student.skills[0]["name"] }}</p>
+    <p>{{ student.skills[1]["name"] }}</p>
+    <p>{{ student.skills[2]["name"] }}</p>
+    <p>{{ student.skills[3]["name"] }}</p>
+    <p>{{ student.skills[4]["name"] }}</p>
+    <p>{{ student.skills[5]["name"] }}</p>
+    <p>{{ student.skills[6]["name"] }}</p>
+    <p>{{ student.skills[7]["name"] }}</p>
+    <p>{{ student.skills[8]["name"] }}</p>
+    <p>{{ student.skills[9]["name"] }}</p>
+    
     <h2>CAPSTONE</h2>
-    <p>{{ capstone.name }}</p>
-    <p>{{ capstone.description }}</p>
-    <p>{{ capstone.url }}</p>
-    <p>{{ capstone.screenshot }}</p>
+    <p>{{ student.capstones[0]["name"] }}</p>
+    <p>{{ student.capstones[0]["description"] }}</p>
+    <p>{{ student.capstones[0]["url"] }}</p>
+    <p>{{ student.capstones[0]["screenshot"] }}</p>
   </div>
 </template>
 
@@ -45,27 +53,12 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "SHOW PAGE",
-      student: {},
-
+      message: "RESUMES",
+      student: [],
       experience: [],
-
-      education: {
-        start_date: "July 26, 2019",
-        end_date: "November 15, 2019",
-        degree: "Bachelor of Science in Noob",
-        university: "University of Noobs",
-        details: "I studied being a noob"
-      },
-      skill: {
-        name: "Being a noob"
-      },
-      capstone: {
-        name: "Noob",
-        description: "Project showcasing how much of a noob I am",
-        URL: "www.capstone.com/noob",
-        screenshot: "picture of noob project here"
-      }
+      education: [],
+      skill: [],
+      capstone: [],
     };
   },
   created: function() {
